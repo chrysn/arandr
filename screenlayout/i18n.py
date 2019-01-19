6ef9f997eec5f8d95177b6bb971244315ee043fb
actionless <actionless.loveless@gmail.com>
2019-01-19 03:22:23 +0100

    style(gui): pylint and so on

---
+++ screenlayout/i18n.py
@@ -0,0 +1,14 @@
+""" This file is licensed under GPLv3, see https://www.gnu.org/licenses/ """
+
+import gettext
+
+
+TRANSLATION = gettext.translation('arandr', fallback=True)
+
+
+def _(msg: str) -> str:
+    return TRANSLATION.gettext(msg)
+
+
+def _n(singular: str, plural: str, count: int) -> str:
+    return TRANSLATION.ngettext(singular, plural, count)
